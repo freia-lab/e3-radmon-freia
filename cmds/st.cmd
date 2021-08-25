aaec7b2287308525af5fc3d121fe8586b5509820
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2021-08-25 13:26:08 +0200

    Added save/restore. Updated opi.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -1,7 +1,8 @@
-#require stream,2.8.10
-require dpu,1.0.0
+require dpu,1.0.1
  
-#Specify the TCP endpoint and give your 'bus' an arbitrary name eg. "asynstream1".
+#require autosave,5.10.2
+ 
+Specify the TCP endpoint and give your 'bus' an arbitrary name eg. "asynstream1".
 drvAsynIPPortConfigure("PORT1","192.168.10.5:4001",0,0,0)
 
 #drvAsynSerialPortConfigure ("asynport2","/dev/ttyS0")
@@ -24,6 +25,12 @@ dbLoadRecords("dpu.db", "PORT=PORT1,P=RadProt-,DET1=Office:GD-01,DET2=Bunker1:GD
 asynSetTraceIOMask "PORT1",0,4   # Enable traceIOHex
 #asynSetTraceMask "PORT1",0,0x1f     # Enable traceError and traceIODriver
 #asynSetTraceMask "PORT1",0,0xfff     # Enable traceError and traceIODriver
+
+epicsEnvSet("TOP", "/opt/epics/autosave")
+epicsEnvSet("IOCNAME", "RadProt-freia")
+epicsEnvSet("IOCDIR", "radmon-freia")
+
+iocshLoad("$(autosave_DIR)/autosave.iocsh", "AS_TOP=$(TOP),IOCNAME=$(IOCNAME)")
 
 iocInit()
 
